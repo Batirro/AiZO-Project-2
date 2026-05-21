6f925893ec74bf565730d90d4d98fad19b99f089
Batirro <batirro34@protonmail.com>
2026-05-21 15:42:55 +0000

    chore: add external CLI arguments library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Parameters.h"
 
-int main(int /*argc*/, char* /*argv*/[]) {
+int main(int argc, char* argv[]) {
     std::cout << "Aizo Project 2 - Graphs" << std::endl;
+    
+    // Inicjalizacja parsera parametrów
+    Parameters::readParameters(argc, argv);
+    
+    // Przykładowe wypisanie
+    // Parameters::printParameters();
+    
     return 0;
 }
