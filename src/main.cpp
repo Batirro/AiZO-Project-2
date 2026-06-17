06c75ecb43d5f4ae3b4f7dc84904831f193bab31
Batirro <batirro34@protonmail.com>
2026-06-17 16:30:34 +0000

    feat: add help mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,53 @@
 #include <iostream>
 #include "Parameters.h"
-#include "AdjacencyList.hpp"
-#include "IncidenceMatrix.hpp"
-#include "GraphGenerator.hpp"
-#include "GraphLoader.hpp"
-#include "PrimMST.hpp"
-#include "KruskalMST.hpp"
-#include "DijkstraSP.hpp"
-#include "BellmanFordSP.hpp"
+
+// Help
+static void printHelp() {
+    std::cout << "Uzycie: ./AizoProject2 [parametry]\n" << std::endl;
+    std::cout << "Pomoc (-h / --help):" << std::endl;
+    std::cout << "Tryby dzialania (-m / --mode):" << std::endl;
+    std::cout << "  singleFile  - Wczytaj graf z pliku i rozwiaz problem" << std::endl;
+    std::cout << "  benchmark   - Przeprowadz badania wydajnosci" << std::endl;
+    std::cout << "Problemy (-p / --problem):" << std::endl;
+    std::cout << "  mst         - Minimalne drzewo rozpinajace" << std::endl;
+    std::cout << "  sp          - Najkrotsza sciezka" << std::endl;
+    std::cout << "  mf          - Maksymalny przeplyw (niezaimplementowany)\n" << std::endl;
+    std::cout << "Algorytmy (-a / --algorithm):" << std::endl;
+    std::cout << "  allAlgorithms - Wszystkie algorytmy dla danego problemu" << std::endl;
+    std::cout << "  prim          - Algorytm Prima (MST)" << std::endl;
+    std::cout << "  kruskal       - Algorytm Kruskala (MST)" << std::endl;
+    std::cout << "  dijkstra      - Algorytm Dijkstry (SP)" << std::endl;
+    std::cout << "  bellmanFord   - Algorytm Forda-Bellmana (SP)\n" << std::endl;
+    std::cout << "Struktury (-s / --structure):" << std::endl;
+    std::cout << "  allStructures   - Obie struktury" << std::endl;
+    std::cout << "  incidenceMatrix - Macierz incydencji" << std::endl;
+    std::cout << "  adjacencyList   - Lista sasiedztwa\n" << std::endl;
+    std::cout << "Parametry trybu singleFile:" << std::endl;
+    std::cout << "  -f <plik>        - Plik wejsciowy z grafem" << std::endl;
+    std::cout << "  --vs <wierzch>   - Wierzcholek startowy (SP)" << std::endl;
+    std::cout << "  --ve <wierzch>   - Wierzcholek koncowy (SP)\n" << std::endl;
+    std::cout << "Parametry trybu benchmark:" << std::endl;
+    std::cout << "  -n <liczba>      - Liczba wierzcholkow" << std::endl;
+    std::cout << "  -d <gestosc>     - Gestosc grafu w % (25/50/75/99)" << std::endl;
+    std::cout << "  -i <iteracje>    - Liczba powtorzen (domyslnie 50)" << std::endl;
+    std::cout << "  -o <plik>        - Plik wyjsciowy CSV z wynikami\n" << std::endl;
+    std::cout << "Przykladowe wywolania:" << std::endl;
+    std::cout << "  ./AizoProject2 -m singleFile -p mst -a prim -s adjacencyList -f graf.txt" << std::endl;
+    std::cout << "  ./AizoProject2 -m benchmark -p sp -a allAlgorithms -s allStructures -n 100 -d 50 -i 50 -o wyniki.csv" << std::endl;
+    std::cout << "  ./AizoProject2 -m help" << std::endl;
+}
 
 int main(int argc, char* argv[]) {
-    std::cout << "Aizo Project 2 - Graphs" << std::endl;
+    Parameters::readParameters(argc - 1, argv + 1);
 
-    Parameters::readParameters(argc, argv);
-
-    std::cout << "\n=== Test algorytmow MST ===\n" << std::endl;
-
-    std::cout << "--- Generowanie grafu testowego ---" << std::endl;
-    GraphGenerator gen(8, 0.4f, false);
-    Graph* graph = gen.generate(true);
-    std::cout << "Graf: " << graph->getVerticesCount()
-              << " wierzcholkow, " << graph->getEdgesCount() << " krawedzi" << std::endl;
-    graph->print();
-
-    std::cout << "\n--- Algorytm Prima ---" << std::endl;
-    MSTResult primResult = PrimMST::findMST(graph);
-    PrimMST::printMST(primResult);
-
-    std::cout << "\n--- Algorytm Kruskala ---" << std::endl;
-    MSTResult kruskalResult = KruskalMST::findMST(graph);
-    KruskalMST::printMST(kruskalResult);
-
-    std::cout << "\n--- Porownanie wynikow ---" << std::endl;
-    std::cout << "Koszt MST (Prim):    " << primResult.totalCost << std::endl;
-    std::cout << "Koszt MST (Kruskal): " << kruskalResult.totalCost << std::endl;
-
-    if (primResult.totalCost == kruskalResult.totalCost) {
-        std::cout << "SUKCES: Oba algorytmy daly ten sam koszt MST!" << std::endl;
-    } else {
-        std::cout << "UWAGA: Rozne koszty - sprawdz implementacje" << std::endl;
+    switch (Parameters::runMode) {
+        case Parameters::RunModes::help:
+            printHelp();
+            break;
+        default:
+            std::cout << "Nie podano trybu. Uzyj -m help aby zobaczyc pomoc." << std::endl;
+            printHelp();
+            break;
     }
-
-    std::cout << "\n--- Test na macierzy incydencji ---" << std::endl;
-    Graph* graphMatrix = gen.generate(false);
-    std::cout << "Graf (macierz): " << graphMatrix->getVerticesCount()
-              << " wierzcholkow, " << graphMatrix->getEdgesCount() << " krawedzi" << std::endl;
-
-    MSTResult primMatrix = PrimMST::findMST(graphMatrix);
-    std::cout << "Prim (macierz) - koszt: " << primMatrix.totalCost << std::endl;
-
-    MSTResult kruskalMatrix = KruskalMST::findMST(graphMatrix);
-    std::cout << "Kruskal (macierz) - koszt: " << kruskalMatrix.totalCost << std::endl;
-
-    std::cout << "\n=== Test algorytmu SP - Dijkstra ===\n" << std::endl;
-
-    GraphGenerator genSP(8, 0.4f, true);
-    Graph* graphSP = genSP.generate(true);
-    std::cout << "Graf skierowany: " << graphSP->getVerticesCount()
-              << " wierzcholkow, " << graphSP->getEdgesCount() << " krawedzi" << std::endl;
-    graphSP->print();
-
-    int startVertex = 0;
-    int endVertex   = graphSP->getVerticesCount() - 1;
-
-    std::cout << "\n--- Dijkstra (lista sasiedztwa) ---" << std::endl;
-    DijkstraSP dijkstra;
-    SPResult dijkstraResult = dijkstra.findSP(graphSP, startVertex);
-    dijkstra.printSP(dijkstraResult, startVertex, endVertex, graphSP->getVerticesCount());
-
-    Graph* graphSPMatrix = genSP.generate(false);
-    std::cout << "\n--- Dijkstra (macierz incydencji) ---" << std::endl;
-    SPResult dijkstraMatrix = dijkstra.findSP(graphSPMatrix, startVertex);
-    dijkstra.printSP(dijkstraMatrix, startVertex, endVertex, graphSPMatrix->getVerticesCount());
-
-    std::cout << "\n--- Bellman-Ford (lista sasiedztwa) ---" << std::endl;
-    SPResult bfResult = BellmanFordSP::findSP(graphSP, startVertex);
-    BellmanFordSP::printSP(bfResult, startVertex, endVertex, graphSP->getVerticesCount());
-
-    std::cout << "\n--- Bellman-Ford (macierz incydencji) ---" << std::endl;
-    SPResult bfMatrix = BellmanFordSP::findSP(graphSPMatrix, startVertex);
-    BellmanFordSP::printSP(bfMatrix, startVertex, endVertex, graphSPMatrix->getVerticesCount());
-
-    // Porownanie
-    std::cout << "\n--- Porownanie SP ---" << std::endl;
-    std::cout << "Koszt SP (Dijkstra):     " << dijkstraResult.dist[endVertex] << std::endl;
-    std::cout << "Koszt SP (Bellman-Ford): " << bfResult.dist[endVertex] << std::endl;
-    if (dijkstraResult.dist[endVertex] == bfResult.dist[endVertex]) {
-        std::cout << "SUKCES: Oba algorytmy daly ten sam koszt!" << std::endl;
-    } else {
-        std::cout << "UWAGA: Rozne koszty - sprawdz implementacje" << std::endl;
-    }
-
-    delete graph;
-    delete graphMatrix;
-
     return 0;
 }
