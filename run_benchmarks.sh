735ee59c30d4d13dde31e09e0282f5efc207d162
Batirro <batirro34@protonmail.com>
2026-06-22 19:47:32 +0000

    chore: run_benchmarks explicitly iterates over each algorithm and structure

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -5,24 +5,41 @@ cd $BUILD
 ITERATIONS=50
 SIZES="10 50 100 200 500"
 DENSITIES="25 50 75 99"
+MST_RESULTS="mst_results.csv"
+SP_RESULTS="sp_results.csv"
+
+# Algorytmy MST: 1=prim, 2=kruskal
+MST_ALGORITHMS="1 2"
+# Algorytmy SP:  3=dijkstra, 4=bellmanFord
+SP_ALGORITHMS="3 4"
+# Struktury: 1=incidenceMatrix, 2=adjacencyList
+STRUCTURES="1 2"
 
 echo "=== MST ==="
-for n in $SIZES; do
-  for d in $DENSITIES; do
-    echo "MST n=$n d=$d"
-    ./AizoProject2 --benchmark --problem 0 --algorithm 0 --structure 0 \
-      --vertexCount $n --density $d --iterations $ITERATIONS \
-      --resultsFile mst_results.csv
+for alg in $MST_ALGORITHMS; do
+  for struct in $STRUCTURES; do
+    for n in $SIZES; do
+      for d in $DENSITIES; do
+        echo "MST alg=$alg struct=$struct n=$n d=$d"
+        ./AizoProject2 --benchmark --problem 0 --algorithm $alg --structure $struct \
+          --vertexCount $n --density $d --iterations $ITERATIONS \
+          --resultsFile "$MST_RESULTS"
+      done
+    done
   done
 done
 
 echo "=== SP ==="
-for n in $SIZES; do
-  for d in $DENSITIES; do
-    echo "SP n=$n d=$d"
-    ./AizoProject2 --benchmark --problem 1 --algorithm 0 --structure 0 \
-      --vertexCount $n --density $d --iterations $ITERATIONS \
-      --resultsFile sp_results.csv
+for alg in $SP_ALGORITHMS; do
+  for struct in $STRUCTURES; do
+    for n in $SIZES; do
+      for d in $DENSITIES; do
+        echo "SP alg=$alg struct=$struct n=$n d=$d"
+        ./AizoProject2 --benchmark --problem 1 --algorithm $alg --structure $struct \
+          --vertexCount $n --density $d --iterations $ITERATIONS \
+          --resultsFile "$SP_RESULTS"
+      done
+    done
   done
 done
 
